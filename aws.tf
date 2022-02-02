0c95fa96f1003184a22c301eba0ee3bea82e7fdc
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2022-02-02 23:15:08 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA4C42SYVUVVYM4VWD"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
 

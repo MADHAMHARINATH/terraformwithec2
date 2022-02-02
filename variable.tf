e179fd3cd085402bae71903e60d46dd45cdd95fc
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2022-02-02 12:49:15 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-2" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "my-jenkins-security-group" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-03a0c45ebc70f98ea" 
+}

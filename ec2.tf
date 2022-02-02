3eb8b382534703cccd93a9b54a1d2ab481fc77e7
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2022-02-02 12:40:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,4 @@
-resource “aws_instance” “testinstance” {
+resource “aws_instance” {
 ami = “ami — 028598a84ca601344”
 instance_type = “t2.micro”
-key_name “testinstance”
-tags {
-Name = “testinstance”
-}
 }

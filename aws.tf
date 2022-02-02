f81c31af78b8178e3c18137677836f5f2515c85d
MADHAMHARINATH <92628693+MADHAMHARINATH@users.noreply.github.com>
2022-02-02 15:30:55 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -3,3 +3,12 @@ provider "aws" {
   secret_key = "zUJaRx3USuEF/FoYQtL/dvSLzBghZL0eokI0OGLb"
   region     = "us-east-1"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e472ba40eb589f49"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
